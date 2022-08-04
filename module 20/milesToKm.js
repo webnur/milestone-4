c9b568190bb84e2e9f08703e3bd9fdb930096e63
webnur <mdnuruli291@gmail.com>
2022-08-05 05:46:56 +0600

    miles to kilometar

---
+++ module 20/milesToKm.js
@@ -0,0 +1,9 @@
+
+function milesToKolometar(miles){
+    const kilometar = miles * 1.609;
+    return kilometar;
+}
+
+// const miles = 10;
+// const totalKm = milesToKolometar(miles)
+// console.log(miles)

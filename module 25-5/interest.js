898fe86cd4bc836e4a8e27275f05295877bd6ea5
webnur <mdnuruli291@gmail.com>
2022-08-06 12:05:43 +0600

    simple interest modify done

--- module 25-5/interest.js
+++ module 25-5/interest.js
@@ -1,16 +1,26 @@
 
 function simpleInterest(principalAmount, years){
 
-    const interestPerYear = 10;
-    const interestAmount = principalAmount / interestPerYear;
-    const totalInterest = interestAmount * years;
-    return totalInterest;
+    const interestRate = 10;
+    const interestAmount = (principalAmount * interestRate * years) /100 ;
+    return interestAmount;
+  
 
 }
 
 const loan = 100000;
 
-const interest = simpleInterest(loan, 2)
+const interest = simpleInterest(loan, 1)
 console.log(interest)
 
 
+// function simpleInterest1 (amout, year){
+//     const munafa = 10;
+//     const interests = (amout * year * munafa) / 100;
+//     return interests
+// }
+
+
+// const amount = 10000;
+// const mainInterest = simpleInterest1(amount, 1)
+// console.log(mainInterest)

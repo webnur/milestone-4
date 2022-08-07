a395c27f33a875683e025c871ac02277cced7b3d
webnur <mdnuruli291@gmail.com>
2022-08-07 10:12:43 +0600

    max and max 3 done

---
+++ module 21/max3.js
@@ -0,0 +1,14 @@
+
+let jim = 89;
+let dela = 99;
+let chinku = 77;
+
+if(jim > dela && jim > chinku){
+    console.log('jim will get the cake')
+}
+else if(dela > jim && dela > chinku){
+    console.log('dela will get the cake')
+}
+else{
+    console.log('chinku will get the cake')
+}

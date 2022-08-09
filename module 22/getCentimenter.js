958be822586bb99b658321cceb5b63a4e736fac7
webnur <mdnuruli291@gmail.com>
2022-08-09 22:45:16 +0600

    paper requirement done

---
+++ module 22/getCentimenter.js
@@ -0,0 +1,7 @@
+function centimeterToMeter(getcentimenter){
+    var totalMiter = getcentimenter / 100;
+    return totalMiter;
+}
+
+var meterResult = centimeterToMeter(1)
+console.log(meterResult)
---
+++ module 22/paper.js
@@ -0,0 +1,17 @@
+
+function paperRequirements (firstBook, secondBook, thridBook) {
+    const firstBookPaper = 100;
+    const secondBookPaper = 200;
+    const thirdBookPaper = 300;
+
+    const firstTotalPaper = firstBookPaper * firstBook;
+    const secondTotalPaper = secondBookPaper * secondBook;
+    const thirdTotalPaper = thirdBookPaper * thridBook;
+
+    const totalPaper =  firstTotalPaper + secondTotalPaper + thirdTotalPaper;
+    return totalPaper;
+}
+
+let totalPapers = paperRequirements(1, 1, 1)
+
+console.log(totalPapers)

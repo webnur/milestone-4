a36a75fdfef815aa277b879c1ea0e13a2485f9cb
webnur <mdnuruli291@gmail.com>
2022-08-10 11:34:08 +0600

    largest number is done

---
+++ module 17/largest.js
@@ -0,0 +1,14 @@
+
+let number1 = 13;
+let number2 = 79;
+let number3 = 105;
+
+if(number1 > number2 && number1 > number3){
+    console.log('number one largest number');
+}
+else if(number2 > number1 && number2 > number3){
+    console.log('number two is largest number')
+}
+else{
+    console.log('number three is largest number')
+}

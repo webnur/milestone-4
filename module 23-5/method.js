1cf3dc4fc0728f68a20375f3647dd69a0fc176df
webnur <mdnuruli291@gmail.com>
2022-08-15 15:14:11 +0600

    module 23-5 done

---
+++ module 23-5/method.js
@@ -0,0 +1,26 @@
+
+const numbers= [45, 65,23, 98, 19];
+
+// for(let i = 0; i < numbers.length; i++){
+//     const number = numbers[i];
+//     console.log(number)
+// }
+
+
+// for of loop
+// for(const number of numbers){
+//     console.log(number)
+// }
+
+const products = [
+    {id: 1, name: 'walton', price: 19000},
+    {id: 2, name: 'walton mobile', price: 22000},
+    {id: 3, name: 'mac book pro', price: 190000},
+    {id: 4, name: 'Iphone', price: 290000},
+    {id: 5, name: 'dell laptop', price: 21000},
+    {id: 6, name: 'hp laptop', price: 34000},
+]
+
+for(const product of products){
+    console.log(product)
+}
